dbf85c6f656ec1c3b42759fd5c74791cc2e51218
Patrick Wojcik <70239167+Pwojcik2@users.noreply.github.com>
2024-10-18 12:42:47 -0400

    coding challenge complete

---
+++ amexChallenge/src/App.jsx
@@ -0,0 +1,70 @@
+import { useState, useEffect} from 'react'
+import './App.css'
+// Build a reload form
+// 1. Current value on this Gift Card is $50, max value to put on a card is $200.
+// 2. Amount options $15, $25, $50, $100, $200. Only show the availble options
+// 3. Select the option, make necessary visual indication
+// 4. Submit and update the value on the card 
+
+// Bonus:
+// 1. Add a custom input, allow user to key in the amount
+
+function App() {
+
+  const [balance, setBalance] = useState(50);
+  const [selectedAmount, setSelectedAmount] = useState(0)
+  const [isSelected, setIsSelected] = useState(false)
+  const maxValue = 200; 
+
+  const amountOptions = [15, 25, 50, 100, 200]
+  // .filter(
+  //   (options) => options + selectedAmount <= maxValue
+  // )
+
+  const handleChange = (options) => {
+    setSelectedAmount(options)
+    setIsSelected(true)
+  }
+
+  const handleSubmit = () => {
+    setBalance(selectedAmount)
+  }
+
+  return (
+    <div style={{ padding: 12 }}>
+      <h1 className="header" style={{ padding: 12 }}>
+        Reload Gift Card
+      </h1>
+      <div
+        style={{
+          border: "1px solid #cecece",
+          height: 120,
+          width: 280,
+          borderRadius: 10,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <h1>
+          ${balance}
+        </h1>
+      </div>
+      <div style={{ display: "flex", padding: '12px 0', gap: 8 }}>
+        {amountOptions.map((options) => (
+          <button
+            style={{backgroundColor: selectedAmount === options ? "#ccc" : "#fff"}}
+           onClick={() => handleChange(options)}>
+            ${options}
+          </button>
+        ))}
+      </div>
+      { isSelected ? <button onClick={handleSubmit}>Submit</button> 
+      : 
+      <button disabled>Submit</button>
+      }
+    </div>
+  );
+};
+
+export default App;
